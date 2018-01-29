a9ca7b0d0cc8ba3079fe5d8336f966e1e001e95f
rex <fenderrex@gmail.com>
2018-01-29 12:01:11 -0800

    s should be a solid start on opto networking mesh

---
+++ Assets/ManagedNetworkMesh.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagedNetworkMesh : MonoBehaviour
+{
+
+    NetworkMesh mesh;
+    // Use this for initialization
+    ManagedNetworkMesh() {
+        mesh = new NetworkMesh();
+
+    }
+    ManagedNetworkMesh(NetworkMesh mesh)
+    {
+        this.mesh = mesh;
+
+    }
+    public int vertexCount
+    {
+        get
+        {
+            return vertexCount;
+        }
+
+    }
+    public int[] triangles
+    {
+        get
+        {
+            return mesh.triangles;
+        }
+        set
+        {
+            mesh.triangles = value;
+        }
+    }
+    public Vector3[] vertices
+    {
+        get
+        {
+            return mesh.vertices;
+        }
+        set
+        {
+            mesh.vertices = value;
+            mesh.vertexCount = vertices.Length;
+        }
+    }
+    public Vector2[] uv
+    {
+        get
+        {
+            return mesh.uv;
+        }
+        set
+        {
+            mesh.uv =value;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/NetworkMesh.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class NetworkMesh : System.Object
+{
+
+    Mesh i = new Mesh();
+    int[] _triangles;
+    Vector3[] _vertices;
+    Vector2[] _uv;
+    int _vertexCount;
+    string _name;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public int vertexCount
+    {
+        get
+        {
+            return vertexCount;
+
+        }
+        set
+        {
+             vertexCount=value;
+        }
+
+    }
+    public int[] triangles
+    {
+        get
+        {
+            return _triangles;
+        }
+        set
+        {
+            _triangles = value;
+        }
+    }
+    public Vector3[] vertices
+    {
+        get
+        {
+            return _vertices;
+        }
+        set
+        {
+            _vertices = value;
+            _vertexCount = vertices.Length;
+        }
+    }
+    public Vector2[] uv
+    {
+        get
+        {
+            return _uv;
+        }
+        set
+        {
+            _uv = value;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
